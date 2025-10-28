eb3a7d2cdc56a0d429229ed010623165a7665113
Omar Villalba <ovillalba@datus.cloud>
2025-10-28 09:07:03 -0400

    Remove clarity from header

--- components/tracking.js
+++ components/tracking.js
@@ -67,13 +67,6 @@
     w.dataLayer.push({ event: 'page_view' });
   } catch (e) {}
 
-  // 8) Microsoft Clarity
-  (function (c, l, a, r, i, t, y) {
-    c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments); };
-    if (document.getElementById('clarity-tag')) return; // prevent double insert
-    t = l.createElement(r); t.id = 'clarity-tag'; t.async = 1; t.src = 'https://www.clarity.ms/tag/' + i;
-    y = l.getElementsByTagName(r)[0]; y.parentNode.insertBefore(t, y);
-  })(window, document, 'clarity', 'script', 'gq9io6nusi');
 })();
 
 
